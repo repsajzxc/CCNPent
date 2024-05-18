712ad7d5ff2e3a21b6ea39945283296cdbe3bbd0
repsajzxc <jaypersaj02@gmail.com>
2024-05-18 12:01:24 +0800

    Add files via upload

---
+++ w22/createDNSZONES.ps1
@@ -0,0 +1,2 @@
+﻿#creating dns domain or dns zone
+Add-DnsServerPrimaryZone -Name "encor21.com" -ZoneFile "encor21.com.dns"
---
+++ w22/creatingdnsrecords.ps1
@@ -0,0 +1,22 @@
+﻿#creating all important zones:
+add-DnsServerResourceRecord -zonename encor21.com -A -name ns -ipv4address 10.21.1.10
+add-DnsServerResourceRecord -zonename encor21.com -A -name ct -ipv4address 10.21.1.2
+add-DnsServerResourceRecord -zonename encor21.com -A -name cb -ipv4address 10.21.1.4
+add-DnsServerResourceRecord -zonename encor21.com -A -name ed -ipv4address 10.21.21.1
+add-DnsServerResourceRecord -zonename encor21.com -A -name cm -ipv4address 10.21.100.8
+add-DnsServerResourceRecord -zonename encor21.com -A -name c1 -ipv4address 10.21.50.6
+add-DnsServerResourceRecord -zonename encor21.com -A -name c2 -ipv4address 10.21.50.8
+add-DnsServerResourceRecord -zonename encor21.com -A -name p1 -ipv4address 10.21.100.101
+add-DnsServerResourceRecord -zonename encor21.com -A -name p2 -ipv4address 10.21.100.102
+add-DnsServerResourceRecord -zonename encor21.com -A -name wc -ipv4address 10.21.10.21
+
+
+
+add-DnsServerResourceRecord -zonename encor21.com -A -name "." -ipv4address 10.21.1.10
+#
+add-DnsServerResourceRecord -zonename encor21.com -Cname -name www -hostname ns.encor21.com
+add-DnsServerResourceRecord -zonename encor21.com -Cname -name pop -hostname ns.encor21.com
+add-DnsServerResourceRecord -zonename encor21.com -Cname -name imap -hostname ns.encor21.com
+add-DnsServerResourceRecord -zonename encor21.com -Cname -name smtp -hostname ns.encor21.com
+#
+Add-DnsServerResourceRecordMX -Preference 10  -Name "." -MailExchange "ns.encor21.com" -ZoneName "encor21.com"
